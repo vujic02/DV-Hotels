e57155147acb4d82be0fb7e120e718b6cade315a
vujic02 <vujicn02@gmail.com>
2021-03-03 00:30:59 +0100

    added navbar features

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,34 @@
-import styled from "styled-components";
+import React from 'react'
+import {
+    Nav, Logo, LinkDiv, Navlink, Button, ButtonHolder, Avatar, MobileIcon
+}  from "../../styles/navbar-styles"
 
-export const Navbar = styled.nav`
-  width: 100%;
-  height: 90px;
-  background-color: var(--fadedWhite);
+const Navbar = () => {
 
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
+    let MainLogo = "./images/logo.png"
+
+    // Make a useState hook for isOpen, when MobileIcon is clicked, toggle it between true and false ( make it in App.js )
+    
+    return (
+        <Nav>
+            <Logo src={MainLogo} />
+            <LinkDiv>
+                <Navlink>Home<div className="underline"></div></Navlink>
+                <Navlink>About<div className="underline"></div></Navlink>
+                <Navlink>Hotels<div className="underline"></div></Navlink>
+                <Navlink>Contact<div className="underline"></div></Navlink>
+            </LinkDiv>
+            <ButtonHolder>
+                <Button>Sign out</Button>
+                <Avatar src={MainLogo} />
+            </ButtonHolder>
+            <MobileIcon isOpen={true}>
+                <div className="line line1"></div>
+                <div className="line line2"></div>
+                <div className="line line3"></div>
+            </MobileIcon>
+      </Nav>
+    )
+}
+
+export default Navbar
---
+++ src/styles/navbar-styles.js
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  width: 100%;
+  height: 90px;
+  background-color: var(--fadedWhite);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+export const Navlink = styled.a`
+  font-size: 1.5rem;
+  font-weight: 500;
+  font-family: Montserrat, sans-serif;
+  color: var(--black);
+  position: relative;
+  overflow: hidden;
+
+  .underline {
+    position: absolute;
+    left: -100%;
+    bottom: 0;
+    background: var(--denimBlue);
+    width: 100%;
+    height: 2px;
+    transition: all ease 0.3s;
+  }
+
+  &:hover {
+    .underline {
+      left: 0;
+    }
+  }
+
+`;
+
+export const Button = styled.button`
+  background-color: var(--black);
+  display: inline-block;
+  padding: .6rem;
+  color: #fff;
+  font-size: 1.2rem;
+  cursor: pointer;
+  outline: none;
+  border: 1px solid transparent;
+  border-radius: 5px;
+
+
+  &:active{
+    transition: 0.2s all ease-in-out;
+    background-color: var(--white);
+    color: var(--black);
+    border: 1px solid var(--black);
+  }
+
+`;
+
+export const ButtonHolder = styled.div`
+  width: 200px;
+  height: 90px;
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const LinkDiv = styled.div`
+  height: 90px;
+  width: 500px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const Avatar = styled.img`
+  border-radius: 30px;
+  width: 50px;
+  height: 50px;
+  background-color: white;
+`;
+
+export const Logo = styled.img`
+  width: 80px;
+  height: 80px;
+`;
+
+export const MobileIcon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.2rem;
+  width: 3rem;
+  height: 3rem;
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  
+  .line {
+    width: 100%;
+    height: .4rem;
+    background: var(--black);
+    transition: 0.3s ease-in-out all;
+  }
+
+  .line1 {
+    transform: ${({isOpen}) => (isOpen ? "translateY(12px) rotate(45deg)" : "" )};
+  }
+
+  .line2 {
+    display: ${({isOpen}) => (isOpen ? "none" : "block")};
+  }
+
+  .line3 {
+    transform: ${({isOpen}) => (isOpen ?  "translateY(-12px) rotate(-45deg)" : "" )};
+  }
+
+`;
