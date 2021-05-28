f7b90dc11536a2b94cd0b189eb8572a36cd5e32e
vujic02 <vujicn02@gmail.com>
2021-05-28 14:31:07 +0200

    added recommended section

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -3,6 +3,7 @@ import SearchBar from "../components/Main/SearchBar";
 import SpecialDeals from "../components/Main/SpecialDeals";
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/Sidebar/Sidebar";
+import Recommended from "../components/Main/Recommended";
 import { Container, TextH1 } from "../styles/global-styles";
 
 const Hotels = ({ isOpen, toggle }) => {
@@ -31,6 +32,16 @@ const Hotels = ({ isOpen, toggle }) => {
         Special Deals
       </TextH1>
       <SpecialDeals />
+      <TextH1
+        opac="0.85"
+        alignText="left"
+        fontS="2rem"
+        fontW="700"
+        m="5% 0 0 5%"
+      >
+        Recommended
+      </TextH1>
+      <Recommended />
     </div>
   );
 };
